polymerdao/ibc-go

add multihop proof logic for ReceivePacket

modules/core/04-channel/keeper/packet.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	"bytes"
 	"strconv"
+	"strings"
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -14,6 +15,7 @@ import (
 	"github.com/cosmos/ibc-go/v6/modules/core/04-channel/types"
 	host "github.com/cosmos/ibc-go/v6/modules/core/24-host"
 	"github.com/cosmos/ibc-go/v6/modules/core/exported"
+	mh "github.com/cosmos/ibc-go/v6/modules/core/multihop"
 )
 
 // SendPacket is called by a module in order to send an IBC packet on a channel.
@@ -207,13 +209,66 @@ func (k Keeper) RecvPacket(
 
 	commitment := types.CommitPacket(k.cdc, packet)
 
-	// verify that the counterparty did commit to sending this packet
-	if err := k.connectionKeeper.VerifyPacketCommitment(
-		ctx, connectionEnd, proofHeight, proof,
-		packet.GetSourcePort(), packet.GetSourceChannel(), packet.GetSequence(),
-		commitment,
-	); err != nil {
-		return sdkerrors.Wrap(err, "couldn't verify counterparty packet commitment")
+	if len(channel.ConnectionHops) > 1 {
+		var proofs types.MsgMultihopProofs
+		if err := k.cdc.Unmarshal(proof, &proofs); err != nil {
+			return err
+		}
+
+		// check all connections are in OPEN state and that the connection IDs match and are in the right order
+		for i, connData := range proofs.ConnectionProofs {
+			var connectionEnd connectiontypes.ConnectionEnd
+			if err := k.cdc.Unmarshal(connData.Value, &connectionEnd); err != nil {
+				return err
+			}
+
+			// Verify the first N-1 connectionHops (last hop already verified above)
+			// 1. check the connectionHop values match the proofs and are in the same order.
+			parts := strings.Split(connData.PrefixedKey.GetKeyPath()[len(connData.PrefixedKey.KeyPath)-1], "/")
+
+			// fmt.Printf("parts[len(parts)-1]: %s\n", parts[len(parts)-1])
+			// fmt.Printf("channel.ConnectionHops[%d]: %s\n", i+1, channel.ConnectionHops[i+1])
+			// fmt.Printf("connectionEnd.Counterparty.ConnectionId: %s\n", connectionEnd.Counterparty.ConnectionId)
+			if parts[len(parts)-1] != channel.ConnectionHops[i+1] {
+				return sdkerrors.Wrapf(
+					connectiontypes.ErrConnectionPath,
+					"connectionHops (%s) does not match connection proof hop (%s) for hop %d",
+					channel.ConnectionHops[i+1], parts[len(parts)-1], i)
+			}
+
+			// 2. check that the connectionEnd's are in the OPEN state.
+			if connectionEnd.GetState() != int32(connectiontypes.OPEN) {
+				return sdkerrors.Wrapf(
+					connectiontypes.ErrInvalidConnectionState,
+					"connection state is not OPEN for connectionID=%s (got %s)",
+					connectionEnd.Counterparty.ConnectionId,
+					connectiontypes.State(connectionEnd.GetState()).String(),
+				)
+			}
+		}
+
+		// get the consensus state at the proofHeight
+		consensusState, found := k.clientKeeper.GetClientConsensusState(ctx, connectionEnd.ClientId, proofHeight)
+		if !found {
+			return sdkerrors.Wrapf(clienttypes.ErrConsensusStateNotFound,
+				"consensus state not found for client id: %s", connectionEnd.ClientId)
+		}
+
+		// verify each consensus state and connection state starting going from Z --> A
+		// finally verify the keyproof on A within B's verified view of A's consensus state.
+		if err := mh.VerifyMultiHopProofMembership(consensusState, k.cdc, &proofs); err != nil {
+			return err
+		}
+	} else {
+
+		// verify that the counterparty did commit to sending this packet
+		if err := k.connectionKeeper.VerifyPacketCommitment(
+			ctx, connectionEnd, proofHeight, proof,
+			packet.GetSourcePort(), packet.GetSourceChannel(), packet.GetSequence(),
+			commitment,
+		); err != nil {
+			return sdkerrors.Wrap(err, "couldn't verify counterparty packet commitment")
+		}
 	}
 
 	switch channel.Ordering {

polymerdao/ibcx-go

modules/core/04-channel/keeper/packet.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	"bytes"
 	"strconv"
+	"strings"
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -14,6 +15,7 @@ import (
 	"github.com/cosmos/ibc-go/v7/modules/core/04-channel/types"
 	host "github.com/cosmos/ibc-go/v7/modules/core/24-host"
 	"github.com/cosmos/ibc-go/v7/modules/core/exported"
+	mh "github.com/cosmos/ibc-go/v7/modules/core/multihop"
 )
 
 // SendPacket is called by a module in order to send an IBC packet on a channel.
@@ -198,13 +200,66 @@ func (k Keeper) RecvPacket(
 
 	commitment := types.CommitPacket(k.cdc, packet)
 
-	// verify that the counterparty did commit to sending this packet
-	if err := k.connectionKeeper.VerifyPacketCommitment(
-		ctx, connectionEnd, proofHeight, proof,
-		packet.GetSourcePort(), packet.GetSourceChannel(), packet.GetSequence(),
-		commitment,
-	); err != nil {
-		return sdkerrors.Wrap(err, "couldn't verify counterparty packet commitment")
+	if len(channel.ConnectionHops) > 1 {
+		var proofs types.MsgMultihopProofs
+		if err := k.cdc.Unmarshal(proof, &proofs); err != nil {
+			return err
+		}
+
+		// check all connections are in OPEN state and that the connection IDs match and are in the right order
+		for i, connData := range proofs.ConnectionProofs {
+			var connectionEnd connectiontypes.ConnectionEnd
+			if err := k.cdc.Unmarshal(connData.Value, &connectionEnd); err != nil {
+				return err
+			}
+
+			// Verify the first N-1 connectionHops (last hop already verified above)
+			// 1. check the connectionHop values match the proofs and are in the same order.
+			parts := strings.Split(connData.PrefixedKey.GetKeyPath()[len(connData.PrefixedKey.KeyPath)-1], "/")
+
+			// fmt.Printf("parts[len(parts)-1]: %s\n", parts[len(parts)-1])
+			// fmt.Printf("channel.ConnectionHops[%d]: %s\n", i+1, channel.ConnectionHops[i+1])
+			// fmt.Printf("connectionEnd.Counterparty.ConnectionId: %s\n", connectionEnd.Counterparty.ConnectionId)
+			if parts[len(parts)-1] != channel.ConnectionHops[i+1] {
+				return sdkerrors.Wrapf(
+					connectiontypes.ErrConnectionPath,
+					"connectionHops (%s) does not match connection proof hop (%s) for hop %d",
+					channel.ConnectionHops[i+1], parts[len(parts)-1], i)
+			}
+
+			// 2. check that the connectionEnd's are in the OPEN state.
+			if connectionEnd.GetState() != int32(connectiontypes.OPEN) {
+				return sdkerrors.Wrapf(
+					connectiontypes.ErrInvalidConnectionState,
+					"connection state is not OPEN for connectionID=%s (got %s)",
+					connectionEnd.Counterparty.ConnectionId,
+					connectiontypes.State(connectionEnd.GetState()).String(),
+				)
+			}
+		}
+
+		// get the consensus state at the proofHeight
+		consensusState, found := k.clientKeeper.GetClientConsensusState(ctx, connectionEnd.ClientId, proofHeight)
+		if !found {
+			return sdkerrors.Wrapf(clienttypes.ErrConsensusStateNotFound,
+				"consensus state not found for client id: %s", connectionEnd.ClientId)
+		}
+
+		// verify each consensus state and connection state starting going from Z --> A
+		// finally verify the keyproof on A within B's verified view of A's consensus state.
+		if err := mh.VerifyMultiHopProofMembership(consensusState, k.cdc, &proofs); err != nil {
+			return err
+		}
+	} else {
+
+		// verify that the counterparty did commit to sending this packet
+		if err := k.connectionKeeper.VerifyPacketCommitment(
+			ctx, connectionEnd, proofHeight, proof,
+			packet.GetSourcePort(), packet.GetSourceChannel(), packet.GetSequence(),
+			commitment,
+		); err != nil {
+			return sdkerrors.Wrap(err, "couldn't verify counterparty packet commitment")
+		}
 	}
 
 	switch channel.Ordering {